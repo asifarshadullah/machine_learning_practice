d8523fe646caa71ada42a25425404747cdaa8889
silent_wraith <asifarshadullah@iut-dhaka.edu>
2019-03-23 14:47:11 +0600

    Feat: Adding total_payment as a new feature

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -11,31 +11,24 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
 features = ["salary", "bonus"]
-#print(data_dict)
-#print('********************')
 cnt = 0
+for point in data_dict:
+    print(point,'  ',data_dict[point],' cnt - ',cnt)
+    cnt+=1
+#print(data_dict)
 data_dict.pop('TOTAL',0)
 data = featureFormat(data_dict, features)
-for point in data:
-    print(point," cnt - ",cnt)
-    cnt+=1
 #print(data)
 #print(len(data))
 ### your code below
 data = np.reshape(np.array(data),(len(data),2))
 #print(data)
 
-cnt = 0
 for point in data:
     salary = point[0]
     bonus = point[1]
-    cnt+=1
-    if (salary>10000000) and (bonus>50000000):
-        print("****************************salary - ",salary," bonus - ",bonus," cnt - ",cnt)
     plt.scatter(salary, bonus)
-    print("salary - ",salary," bonus - ",bonus," cnt - ",cnt)
 
-print(cnt)
 plt.xlabel("salary")
 plt.ylabel("bonus")
 plt.show()
